fbd00cac20ccb9d23f1a353037937f77ca8de275
Dalveer Singh <dalveersidhu97@gmail.com>
2024-07-31 19:56:06 -0600

    added backtracking

--- algorithms/backtracking/index.ts
+++ algorithms/backtracking/index.ts
@@ -1,34 +1,64 @@
+type State = [number[], number[]];
+
+const isValid = (element: number) => {
+    return true;
+}
+
+const statisfyConstraints = ([current, remaining]: State) => {
+    return true;
+}
+
+const isSolution = (current: number[], nums: number[]) => {
+    if (current.length === nums.length) return true;
+    return false;
+}
 
 export const algorithm = (nums: number[]) => {
-    const length = nums.length;
-    const stack: [number[], number[]][] = [[[], nums.slice()]];
+    const stack: State[] = [[[], nums.slice()]];
     const result: number[][] = [];
 
-    const generate_candidates = () => {
-
-    }
-    const backtrack = () => {
-
-    }
-    const isValid = () => {
-
-    }
-    
     while (stack.length > 0) {
-        const [current, remaining] = stack.pop()!;
+        const state = stack.pop()!;
+        const [current, remaining] = state;
 
-        if (current.length === length) {
-            result.push(current.slice());
-            continue;
-        }
-
-        remaining.forEach((element, i) => {
-            stack.push([
+        for (let i = 0; i < remaining.length; i++) {
+            const element = remaining[i];
+            if (!isValid(element))
+                continue;
+            const nextState: State = [
                 [...current, element],
                 [...remaining.slice(0, i), ...remaining.slice(i + 1)]
-            ])
-        });
+            ];
+            if (isSolution(nextState[0], nums))
+                result.push(nextState[0])
+            else if (statisfyConstraints(nextState))
+                stack.push(nextState)
+        }
     }
+    console.log(result);
+    return result;
+}
+
+export const algorithmRecursive = (nums: number[]) => {
+    const initialState: State = [[], nums.slice()];
+    const result: number[][] = [];
+
+    const recurse = ([current, remaining]: State) => {
+        for (let i = 0; i < remaining.length; i++) {
+            const element = remaining[i];
+            if (!isValid(element))
+                continue;
+            const nextState: State = [
+                [...current, element],
+                [...remaining.slice(0, i), ...remaining.slice(i + 1)]
+            ];
+            if (isSolution(nextState[0], nums))
+                result.push(nextState[0])
+            else if (statisfyConstraints(nextState))
+                recurse(nextState)
+        }
+    }
+    recurse(initialState)
     console.log(result);
     return result;
 }
